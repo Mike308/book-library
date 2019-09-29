886f3b1125a5e1698fa0e503b612ad282151839b
Mikołaj Werner <m.werner9205@gmail.com>
2019-09-29 19:00:40 +0200

    added volume info model

---
+++ src/main/java/com/homework/homework/model/VolumeInfo.java
@@ -0,0 +1,25 @@
+package com.homework.homework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VolumeInfo {
+    private String title;
+    private String subtitle;
+    private String publisher;
+    private long publishedData;
+    private String description;
+    private int pageCount;
+    private ImageLink imageLinks;
+    private String language;
+    private String previewLink;
+    private double averageRating;
+    private List<String> authors;
+    private List<String> categories;
+    private List<IndustryIdentifier> industryIdentifiers;
+}
