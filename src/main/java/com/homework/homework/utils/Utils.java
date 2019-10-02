0d7609d4256959d21bd6ce217d88e0be4c5ade8d
Mikołaj Werner <m.werner9205@gmail.com>
2019-10-02 21:34:52 +0200

    implemented method for checking if input string is containing specific keyword

--- src/main/java/com/homework/homework/utils/Utils.java
+++ src/main/java/com/homework/homework/utils/Utils.java
@@ -1,9 +1,15 @@
 package com.homework.homework.utils;
 
+import org.ahocorasick.trie.Emit;
+import org.ahocorasick.trie.Trie;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
 
@@ -18,5 +24,22 @@ public class Utils {
             e.printStackTrace();
             return 0;
         }
+    }
+
+    public static  boolean customContains(String inputString, String keyword) {
+//        Trie trie = Trie.builder().ignoreCase().onlyWholeWords().ignoreOverlaps().addKeywords(keyword.split(" ")).build();
+//        Collection<Emit> emits = trie.parseText(inputString);
+//        boolean found = false;
+//        for (Emit emit : emits) {
+//            System.out.println(emit.get);
+//            found = true;
+//        }
+//        return found;
+        List<String> inputStringList = Arrays.asList(inputString.toUpperCase().replaceAll("[^A-Za-z0-9]", " ").split(" ")).stream()
+                .map(s -> s.replaceAll("\\s+", "")).collect(Collectors.toList());
+        List<String> keyWords = Arrays.asList(keyword.toUpperCase().replaceAll("[^A-Za-z0-9]", " ").split(" ")).stream()
+                .map(s -> s.replaceAll("\\s+", "")).collect(Collectors.toList());
+        inputStringList.forEach(System.out::println);
+        return keyWords.stream().allMatch(inputStringList::contains);
     }
 }
