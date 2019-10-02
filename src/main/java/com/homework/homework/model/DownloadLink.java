31fe67f32a51d0714bad1fc7aa40f25d5bee7a7c
Mikołaj Werner <m.werner9205@gmail.com>
2019-10-02 17:05:33 +0200

    added Download Link model

---
+++ src/main/java/com/homework/homework/model/DownloadLink.java
@@ -0,0 +1,13 @@
+package com.homework.homework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DownloadLink {
+    private String type;
+    private String link;
+}
