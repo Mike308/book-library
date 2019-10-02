41f8800f10c63dd08788e56987ccb7022d24ab63
Mikołaj Werner <m.werner9205@gmail.com>
2019-10-02 21:39:12 +0200

    improved json parser

--- src/main/java/com/homework/homework/utils/JsonParser.java
+++ src/main/java/com/homework/homework/utils/JsonParser.java
@@ -1,19 +1,15 @@
 package com.homework.homework.utils;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.homework.homework.model.Book;
 import com.homework.homework.model.JsonFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class JsonParser {
 
-    public static List<Book> parseJson(String path) {
+    public static JsonFile parseJson(String path) {
         JsonFile jsonFile = new JsonFile();
         ObjectMapper jsonFileMapper = new ObjectMapper();
         jsonFileMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -23,6 +19,6 @@ public class JsonParser {
         }catch (IOException  e) {
             e.printStackTrace();
         }
-        return jsonFile.getItems();
+        return jsonFile;
     }
 }
