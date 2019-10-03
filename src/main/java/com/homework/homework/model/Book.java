2b2b0b512081d9f1ffd1dcc290c71ae9fe3bd1aa
Mikołaj Werner <mikwer001@utp.edu.pl>
2019-10-03 13:30:34 +0200

    added excluding field with default values

--- src/main/java/com/homework/homework/model/Book.java
+++ src/main/java/com/homework/homework/model/Book.java
@@ -1,6 +1,7 @@
 package com.homework.homework.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties({"accessInfo"})
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Book {
     private String isbn;
     private String title;
