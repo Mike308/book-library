2e64e6d113f80b1205b08108ea734a39deca1dc2
Mikołaj Werner <m.werner9205@gmail.com>
2019-10-03 19:53:54 +0200

    removed method for fetching all books added pre-fetching all books in constructor code refactored

--- src/main/java/com/homework/homework/service/impl/BookServiceImpl.java
+++ src/main/java/com/homework/homework/service/impl/BookServiceImpl.java
@@ -14,33 +14,32 @@ public class BookServiceImpl implements BookService {
 
     private JsonFile jsonFile;
     private CustomMapper customMapper = new CustomMapper();
+    private List<Book> allBooks;
 
 
     public BookServiceImpl() {
-        jsonFile = Utils.parseJson(Utils.readConfiguration());
+        jsonFile = Utils.parseJson(Utils.readConfiguration()); //parsing json file to object JsonFile
+        allBooks = customMapper.mapper(jsonFile); //fetch all books from json file
     }
 
-
-    @Override
-    public List<Book> getAllBooks() {
-         return customMapper.mapper(jsonFile);
-    }
 
     @Override
     public Book getSpecifiedBookByISBN(String id) {
-        return getAllBooks().stream().filter(book -> book != null) //eliminate null book object (preventing before NullPointerException)
+        return allBooks.stream().filter(book -> book != null) //eliminate null book object (preventing before NullPointerException)
                 .filter(specifiedBook -> id.equals(specifiedBook.getIsbn())).findAny().orElse(new Book());
     }
 
     @Override
     public List<Book> getBooksByCategory(String category) {
-        return getAllBooks().stream().filter(book -> book != null).filter(book -> book.getCategories() != null)
-                .filter(book -> book.getCategories().contains(category)).collect(Collectors.toList());
+        return allBooks.stream().filter(book -> book != null).filter(book -> book.getCategories() != null)
+                .filter(book ->
+                    book.getCategories().stream().map(cat -> cat.toUpperCase()).collect(Collectors.toList()).contains(category.toUpperCase())
+                ).collect(Collectors.toList());
     }
 
     @Override
     public List<Book> getBooks(String query) {
-        return  getAllBooks().stream().filter(book -> book != null)
+        return  allBooks.stream().filter(book -> book != null)
                 .filter(book -> Utils.customContains(book.toString(), query)).collect(Collectors.toList());
     }
 
@@ -48,11 +47,11 @@ public class BookServiceImpl implements BookService {
     public List<Rating> getRatings() {
         List<String> authors = new ArrayList<>();
         List<Rating> ratings = new ArrayList<>();
-        getAllBooks().stream().filter(book -> book != null).filter(book -> book.getAuthors() != null)
+        allBooks.stream().filter(book -> book != null).filter(book -> book.getAuthors() != null)
                 .filter(book -> book.getAverageRating() > 0.0) //eliminating non rated books
                 .forEach(book -> authors.addAll(book.getAuthors())); //fetch and add authors to authors list
         authors.stream().filter(v -> v != null).distinct().forEach(author -> { //iterate through authors list and filter book belongs to author
-            OptionalDouble averageOptional = getAllBooks().stream().filter(book -> book != null)
+            OptionalDouble averageOptional = allBooks.stream().filter(book -> book != null)
                     .filter(book -> book.getAuthors() != null)
                     .filter(book -> book.getAuthors().contains(author))
                     .mapToDouble(t -> t.getAverageRating()).average(); //mapping book to average rating and calculating average for each author
@@ -65,7 +64,7 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<DownloadLink> getDownloadLinks(String isbn) {
         List<DownloadLink> downloadLinks = new ArrayList<>(); //create list of download links
-        getAllBooks().stream().filter(item -> item != null).filter(item -> isbn.equals(item.getIsbn()))
+        allBooks.stream().filter(item -> item != null).filter(item -> isbn.equals(item.getIsbn()))
                      .filter(item -> item.getAccessInfo() != null).forEach(item -> { //filter all books containing access info and insert links into download links list
             if (item.getAccessInfo().getEpub().isAvailable()){
                 downloadLinks.add(new DownloadLink("epub", item.getAccessInfo().getEpub().getAcsTokenLink()));
